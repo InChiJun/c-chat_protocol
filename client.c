098b908661d9899573a9db8c98cc36808b10d424
edgegg <ske4715@gmail.com>
2024-04-02 16:19:38 +0900

    feat: send 스레드 함수 추가

--- client.c
+++ client.c
@@ -11,17 +11,17 @@
 #define BUFFSIZE 100
 #define NAMESIZE 20
 
-int StoHEX(char fi, char sc);
 void error_handling(char* msg);
 void* send_message(void* arg);
 void* recv_message(void* arg);
 
 char message[BUFFSIZE];
+char id[NAMESIZE];
 
 void* rcv(void* arg){
     printf("rcv thread created!\n");
     
-    int sock = (int)arg;
+    int sock = *((int*)arg);
     char buff[500];
     int len;
 
@@ -40,13 +40,32 @@ void* rcv(void* arg){
     return 0;
 }
 
+void* snd(void* arg)
+{
+    int sock = *((int *)arg);
+    char chat[BUFFSIZE];
+    char msg[NAMESIZE + BUFFSIZE + 4];
+
+    // send 기능 시작 부분. 아래의 내용을 snd thread 만들어야 함
+    printf("while before\n");
+    while(1){
+        fgets(chat, BUFFSIZE, stdin);
+
+        sprintf(msg, "[%s]: %s\n", id, chat);
+        printf("send: %s", msg);
+
+        write(sock, msg, strlen(msg)+1);
+        sleep(1);
+    }
+    printf("while end");
+    return NULL;
+}
 int main(int argc, char **argv){
     int sock;
     struct sockaddr_in serv_addr;
     pthread_t snd_thread, rcv_thread; // **snd_thread는 따로 만들어야 함
     void* thread_result;
 
-    char id[100];
     printf("argc: %d\n", argc); // 처음에 ID값 줬는지 확인하는 라인
     if (argc < 2){
         printf("you have to enter ID\n");
@@ -67,7 +86,7 @@ int main(int argc, char **argv){
     memset(&serv_addr, 0,sizeof(serv_addr));
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    serv_addr.sin_port=htons(7090);
+    serv_addr.sin_port=htons(7989);
 
     if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
         printf("connect error\n");
@@ -75,24 +94,11 @@ int main(int argc, char **argv){
         printf("connection success\n");
     }
 
-    pthread_create(&rcv_thread, NULL, rcv, (void*)sock);
+    pthread_create(&rcv_thread, NULL, rcv, (void*) &sock);
+    pthread_create(&snd_thread, NULL, snd, (void*) &sock);
 
-    char chat[100];
-    char msg[1000];
-
-    // send 기능 시작 부분. 아래의 내용을 snd thread 만들어야 함
-    printf("while before\n");
-    while(1){
-        printf("채팅 입력: "); // 덮어써지는 오류로 인해 해당 라인 삭제 예정
-        gets(chat);
-
-        sprintf(msg, "[%s]: %s\n", id, chat);
-        printf("send: %s", msg);
-
-        write(sock, msg, strlen(msg)+1);
-        sleep(1);
-    }
-    printf("while end");
+    pthread_join(rcv_thread, NULL);
+    pthread_join(snd_thread, NULL);
 
     close(sock);
     return 1;    
