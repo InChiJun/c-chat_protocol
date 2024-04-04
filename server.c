b10d0d3e766ed0377a520d0fc561bfa6134201f9
edgegg <ske4715@gmail.com>
2024-04-04 11:31:13 +0900

    feat: 개인채팅 기능 구현 중

--- server.c
+++ server.c
@@ -11,10 +11,12 @@
 #define CLNT_MAX 10
 #define BUFFSIZE 200
 #define NAMESIZE 20
+#define ROOM_MAX 5
 
 int g_clnt_socks[CLNT_MAX]; // client 소켓을 모아놓은 배열
 int g_clnt_count = 0; // client 수
 pthread_mutex_t g_mutex;
+int g_room_count = 0;   // room 개수
 
 // USER 정보 구조체
 typedef struct _USER
@@ -22,10 +24,19 @@ typedef struct _USER
     char name[NAMESIZE];    // USER NAME
 }USER;
 
+//chatroom 정보 구조체
+typedef struct _ROOM
+{
+    char user1_name[NAMESIZE];
+    char user2_name[NAMESIZE];
+
+}ROOM;
+
+// 구조체 배열들
 USER user[CLNT_MAX];    // user 정보 구조체 배열
+ROOM room[ROOM_MAX];    // room 정보 구조체 배열
 
-
-void send_all_clnt(char* msg, int my_sock){
+void send_all_clnt(char* msg, int my_sock){ // 자신을 제외한 모든 사용자에게 메시지를 전달하는 함수
     pthread_mutex_lock(&g_mutex);
     for(int i = 0; i < g_clnt_count; i++){ // g_clnt_count만큼 반복하기 때문에 send_all이 되는 것.
         if(g_clnt_socks[i] != my_sock){
@@ -108,6 +119,65 @@ void* clnt_connection(void* arg){ // 클라이언트 connect하는 start_routine
     close(clnt_sock);
     pthread_exit(0);
     
+    return NULL;
+}
+
+void* handle_chatroom(void* arg){
+    ROOM room = *((ROOM*)arg);
+    int str_len = 0;
+    char msg[BUFFSIZE];
+    int my_num;
+    
+    // 본인 인덱스 찾는 부분
+    for(int i = 0; i < g_clnt_count; i++){
+        if(clnt_sock == g_clnt_socks[i]){
+            my_num = i;
+            break;
+        }//end if
+    }//end for
+
+    char cmpmsg[NAMESIZE + 8];
+    sprintf(cmpmsg, "[%s]: /msg", user[my_num].name);
+
+    while(1){
+        str_len = read(clnt_sock, msg, sizeof(msg));
+        if(str_len <= 0){
+            printf("clnt[%d] close\n", clnt_sock);
+            break;
+        }
+        if(strncmp(msg, cmpmsg, strlen(cmpmsg)) == 0) send_spec_clnt(msg);
+        else send_all_clnt(msg, clnt_sock); // **소켓을 넣어서 보내는 이유를 파악해보자.
+        printf("%s\n", msg); // **사용자 아이디도 있어야 하므로 버그 가능(수정 필요)
+    }
+    
+    pthread_mutex_lock(&g_mutex);
+    for(; my_num < g_clnt_count - 1; my_num++){
+                g_clnt_socks[my_num] = g_clnt_socks[my_num+1];
+    }//end for
+
+    /*
+    for(i = 0; i < g_clnt_count; i++){
+        if(clnt_sock == g_clnt_socks[i]){
+            for(; i < g_clnt_count - 1; i++){
+                g_clnt_socks[i] = g_clnt_socks[i+1];
+                break;
+            }//end for
+        }//end if
+    }//end for
+    */
+    pthread_mutex_unlock(&g_mutex);
+    pthread_exit(0);
+
+    return NULL;
+}
+
+void create_chatroom(pthread_t* t_thread, char* user1_name, char* user2_name){   // 개인 채팅방 만드는 함수
+    int room_count = 0; // 만들 방의 인덱스 위치
+    pthread_mutex_lock(&g_mutex);
+    // 개인 채팅방 생성 로직을 구현하기
+    if(g_room_count == ROOM_MAX)
+    pthread_create(t_thread, NULL, handle_chatroom, (void*) &room[room_count]);
+    pthread_mutex_unlock(&g_mutex);
     return NULL;
 }
 
